98479ddd594d4e6e5cfef01470c5252baca22c4f
Dhanush Surya S <ds16062003@gmail.com>
2026-09-22 18:50:04 +0530

    JS Varialbles

---
+++ JS/Day 2/assets/js/index.js
@@ -0,0 +1,55 @@
+document.write("Go to Console");
+
+var userName = "Dhanush Surya";
+var cityName = "Chennai";
+let jobTitle = "Developer";
+let teamName = "ByteBuilders";
+let fileName = "index.js";
+const companyName = "SLA";
+const countryName = "India";
+const skillName = "JavaScript";
+const roleName = "Java Dev";
+const projectName = "Learn JS";
+
+
+var userAge = 23;
+var totalScore = 95;
+let itemCount = 10;
+let pageNumber = 1;
+let orderCount = 4;
+const maxLimit = 100;
+const minValue = 0;
+const runRate = 8;
+const yearValue = 2026;
+const idNumber = 7384;
+
+
+var isActive = true;
+var isLoggedIn = false;
+let hasAccess = true;
+let isValid = false;
+let isReady = true;
+const isAdmin = false;
+const isDone = true;
+const hasError = false;
+
+
+var lastBall = null;
+let userToken = null;
+let selectedItem = null;
+const emptyValue = null;
+const noData = null;
+
+
+var tempValue;
+let futureAim;
+let extraInfo;
+const notDef = undefined;
+let pendingTask;
+
+
+console.log(userName);   
+console.log(userAge);    
+console.log(isActive);   
+console.log(lastBall); 
+console.log(tempValue);  
