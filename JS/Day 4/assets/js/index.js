f0c8d1fc714b917287edf102f1b3b33861cd58ff
Dhanush Surya S <ds16062003@gmail.com>
2026-09-24 20:03:47 +0530

    JS Day 4

---
+++ JS/Day 4/assets/js/index.js
@@ -0,0 +1,111 @@
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+}
+
+for (let i = 0; i <= 20; i += 2) {
+    console.log(i);
+}
+
+for (let i = 20; i >= 0; i -= 2) {
+    console.log(i);
+}
+
+for (let i = 1; i <= 30; i += 3) {
+    console.log(i);
+}
+
+for (let i = 30; i >= 1; i -= 3) {
+    console.log(i);
+}
+
+for (let i = 5; i <= 50; i += 5) {
+    console.log(i);
+}
+
+for (let i = 50; i >= 5; i -= 5) {
+    console.log(i);
+}
+
+for (let i = 0; i <= 100; i += 10) {
+    console.log(i);
+}
+
+for (let i = 100; i >= 0; i -= 10) {
+    console.log(i);
+}
+
+for (let i = -10; i <= 10; i++) {
+    console.log(i);
+}
+
+for (let i = 10; i >= -10; i--) {
+    console.log(i);
+}
+
+for (let i = 1; i <= 20; i += 4) {
+    console.log(i);
+}
+
+for (let i = 20; i >= 1; i -= 4) {
+    console.log(i);
+}
+
+for (let i = 2; i <= 40; i += 2) {
+    console.log(i);
+}
+
+for (let i = 40; i >= 2; i -= 2) {
+    console.log(i);
+}
+
+for (let i = 100; i >= 1; i--) {
+    console.log(i);
+}
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+}
+
+for (let i = 3; i <= 60; i += 3) {
+    console.log(i);
+}
+
+for (let i = 60; i >= 3; i -= 3) {
+    console.log(i);
+}
+
+for (let i = 0; i <= 9; i++) {
+    console.log(i);
+}
+
+for (let i = 9; i >= 0; i--) {
+    console.log(i);
+}
+
+for (let i = 1; i <= 15; i += 2) {
+    console.log(i);
+}
+
+for (let i = 15; i >= 1; i -= 2) {
+    console.log(i);
+}
+
+for (let i = 6; i <= 66; i += 6) {
+    console.log(i);
+}
+
+for (let i = 66; i >= 6; i -= 6) {
+    console.log(i);
+}
+
+for (let i = 7; i <= 70; i += 7) {
+    console.log(i);
+}
+
+for (let i = 70; i >= 7; i -= 7) {
+    console.log(i);
+}
